32433bf9f7217db0c44101245a5502e457f3dad4
Umutcan Sahin <umutcan199726@gmail.com>
2023-06-05 22:51:14 +0300

    move timer setup from fragment to viewmodel

--- app/src/main/java/com/umutcansahin/manageyourtime/ui/stopwatch_screen/StopWatchFragment.kt
+++ app/src/main/java/com/umutcansahin/manageyourtime/ui/stopwatch_screen/StopWatchFragment.kt
@@ -1,13 +1,11 @@
 package com.umutcansahin.manageyourtime.ui.stopwatch_screen
 
 import android.os.Bundle
-import android.os.CountDownTimer
 import android.view.View
 import androidx.navigation.fragment.findNavController
 import com.umutcansahin.manageyourtime.R
 import com.umutcansahin.manageyourtime.base.BaseFragment
-import com.umutcansahin.manageyourtime.common.THOUSAND
-import com.umutcansahin.manageyourtime.common.convertToMinuteAndSecond
+import com.umutcansahin.manageyourtime.common.collectFlow
 import com.umutcansahin.manageyourtime.databinding.FragmentStopWatchBinding
 import org.koin.androidx.viewmodel.ext.android.activityViewModel
 
@@ -15,14 +13,17 @@ class StopWatchFragment :
     BaseFragment<FragmentStopWatchBinding>(FragmentStopWatchBinding::inflate) {
 
     private val viewModel by activityViewModel<StopWatchViewModel>()
-    private var countDownTimer: CountDownTimer? = null
-    private var isTimerRunning: Boolean = false
-
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        binding.textViewTime.text = viewModel.timerStartValue.convertToMinuteAndSecond()
+        observeData()
         initUi()
+    }
+
+    private fun observeData() {
+        collectFlow(viewModel.timer) {
+            binding.textViewTime.text = it
+        }
     }
 
     private fun initUi() {
@@ -32,60 +33,19 @@ class StopWatchFragment :
                 findNavController().navigate(R.id.action_stopWatchFragment_to_homeFragment)
             }
             imageButtonStartTimer.setOnClickListener {
-                startTimer()
+                viewModel.startTimer()
             }
             imageButtonStopTimer.setOnClickListener {
-                pauseTimer()
+                viewModel.pauseTimer()
             }
             imageButtonResetTimer.setOnClickListener {
-                resetTimer()
+                viewModel.resetTimer()
             }
         }
-    }
-
-    private fun startTimer() {
-        if (!isTimerRunning) {
-            timer()
-            isTimerRunning = true
-        }
-    }
-
-    private fun pauseTimer() {
-        countDownTimer?.cancel()
-        isTimerRunning = false
-    }
-
-    private fun resetTimer() {
-        if (countDownTimer != null) {
-            countDownTimer!!.cancel()
-            countDownTimer = null
-            isTimerRunning = false
-            viewModel.timerStartValue = 0
-        }
-        binding.textViewTime.text = viewModel.timerStartValue.convertToMinuteAndSecond()
-    }
-
-    private fun timer() {
-        countDownTimer = object : CountDownTimer(
-            Long.MAX_VALUE,
-            Long.THOUSAND
-        ) {
-            override fun onTick(millisUntilFinished: Long) {
-                viewModel.timerStartValue += 1000
-                binding.textViewTime.text = viewModel.timerStartValue.convertToMinuteAndSecond()
-            }
-
-            override fun onFinish() {}
-        }.start()
     }
 
     override fun onStop() {
-        pauseTimer()
+        viewModel.pauseTimer()
         super.onStop()
-    }
-
-    override fun onDestroyView() {
-        countDownTimer?.cancel()
-        super.onDestroyView()
     }
 }
--- app/src/main/java/com/umutcansahin/manageyourtime/ui/stopwatch_screen/StopWatchViewModel.kt
+++ app/src/main/java/com/umutcansahin/manageyourtime/ui/stopwatch_screen/StopWatchViewModel.kt
@@ -1,7 +1,53 @@
 package com.umutcansahin.manageyourtime.ui.stopwatch_screen
 
+import android.os.CountDownTimer
 import androidx.lifecycle.ViewModel
+import com.umutcansahin.manageyourtime.common.THOUSAND
+import com.umutcansahin.manageyourtime.common.convertToMinuteAndSecond
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.asSharedFlow
 
-class StopWatchViewModel: ViewModel() {
-    var timerStartValue: Long = 0
+class StopWatchViewModel : ViewModel() {
+    private var timerStartValue: Long = 0
+    private var countDownTimer: CountDownTimer? = null
+    private var isTimerRunning: Boolean = false
+
+    private val _timer = MutableStateFlow(timerStartValue.convertToMinuteAndSecond())
+    val timer = _timer.asSharedFlow()
+
+    fun startTimer() {
+        if (!isTimerRunning) {
+            timer()
+            isTimerRunning = true
+        }
+    }
+
+    fun pauseTimer() {
+        countDownTimer?.cancel()
+        isTimerRunning = false
+    }
+
+    fun resetTimer() {
+        if (countDownTimer != null) {
+            countDownTimer!!.cancel()
+            countDownTimer = null
+            isTimerRunning = false
+            timerStartValue = 0
+            _timer.value = timerStartValue.convertToMinuteAndSecond()
+        }
+    }
+
+    private fun timer() {
+        countDownTimer = object : CountDownTimer(
+            Long.MAX_VALUE,
+            Long.THOUSAND
+        ) {
+            override fun onTick(millisUntilFinished: Long) {
+                timerStartValue += Long.THOUSAND
+                _timer.value = timerStartValue.convertToMinuteAndSecond()
+            }
+
+            override fun onFinish() {}
+        }.start()
+    }
 }
