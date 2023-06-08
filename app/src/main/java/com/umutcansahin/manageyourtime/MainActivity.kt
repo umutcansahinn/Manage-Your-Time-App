98ce2d79569b9c82b8131eaafeba677102c2cf34
Umutcan Sahin <umutcan199726@gmail.com>
2023-06-08 15:32:21 +0300

    add google admob

--- app/src/main/java/com/umutcansahin/manageyourtime/MainActivity.kt
+++ app/src/main/java/com/umutcansahin/manageyourtime/MainActivity.kt
@@ -2,10 +2,30 @@ package com.umutcansahin.manageyourtime
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import com.google.android.gms.ads.*
+import com.google.android.gms.ads.interstitial.InterstitialAd
+import com.google.android.gms.ads.interstitial.InterstitialAdLoadCallback
+import com.umutcansahin.manageyourtime.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    //ca-app-pub-5509612590921982~6834076783 --> app id
+    //ca-app-pub-5509612590921982~6834076783 --> admob uygulama kimliği
+    //ca-app-pub-5509612590921982/7846979397 --> admob reklam kimliği
+    //ca-app-pub-3940256099942544/6300978111 --> test2
+    //ca-app-pub-3940256099942544~3347511713 --> test1
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        MobileAds.initialize(this) {}
+
+        val adRequest = AdRequest.Builder().build()
+        binding.adView.loadAd(adRequest)
     }
 }
--- app/src/main/java/com/umutcansahin/manageyourtime/ui/home_screen/HomeFragment.kt
+++ app/src/main/java/com/umutcansahin/manageyourtime/ui/home_screen/HomeFragment.kt
@@ -1,17 +1,38 @@
 package com.umutcansahin.manageyourtime.ui.home_screen
 
 import android.os.Bundle
+import android.util.Log
 import android.view.View
 import androidx.navigation.fragment.findNavController
+import com.google.android.gms.ads.AdRequest
+import com.google.android.gms.ads.LoadAdError
+import com.google.android.gms.ads.interstitial.InterstitialAd
+import com.google.android.gms.ads.interstitial.InterstitialAdLoadCallback
 import com.umutcansahin.manageyourtime.base.BaseFragment
 import com.umutcansahin.manageyourtime.common.NavType
 import com.umutcansahin.manageyourtime.databinding.FragmentHomeBinding
 
 class HomeFragment : BaseFragment<FragmentHomeBinding>(FragmentHomeBinding::inflate) {
 
+    private var mInterstitialAd: InterstitialAd? = null
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         initView()
+        showAdd()
+    }
+
+    private fun showAdd() {
+        val adRequest = AdRequest.Builder().build()
+        InterstitialAd.load(requireActivity(),"ca-app-pub-3940256099942544/1033173712", adRequest, object : InterstitialAdLoadCallback() {
+            override fun onAdFailedToLoad(adError: LoadAdError) {
+                mInterstitialAd = null
+            }
+
+            override fun onAdLoaded(interstitialAd: InterstitialAd) {
+
+                mInterstitialAd = interstitialAd
+            }
+        })
     }
 
     private fun initView() {
@@ -41,6 +62,14 @@ class HomeFragment : BaseFragment<FragmentHomeBinding>(FragmentHomeBinding::infl
                     HomeFragmentDirections.actionHomeFragmentToStopWatchFragment()
                 )
             }
+        }
+    }
+    override fun onStart() {
+        super.onStart()
+        if (mInterstitialAd != null) {
+            mInterstitialAd?.show(requireActivity())
+        } else {
+            Log.d("TAG", "The interstitial ad wasn't ready yet.")
         }
     }
 }
